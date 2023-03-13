1940f3fb840572c41a739d7488b518557d6b7667
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-03-13 21:36:53 +0000

    lab2 complete

--- lab2/LegoMosaics.java
+++ lab2/LegoMosaics.java
@@ -5,40 +5,62 @@ import java.util.Arrays;
 
 public class LegoMosaics {
 	
-	private static final int[] LEGO_LENGTHS = {1, 2, 3, 4, 6, 8, 10, 12, 16};
+	private static final int[] LEGO_LENGTHS = {16, 12, 10, 8, 6, 4, 3, 2, 1};
+	private static int[] ways;
+	private static int highestLengthCalculated = 1;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int[] dimensions = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		ways = new int[dimensions[1] + 1];
+		ways[0] = 1;
+		ways[1] = 1;
 		String[] mosaic = new String[dimensions[0]];
 		
 		for (int i = 0; i < dimensions[0]; i++) {
 			mosaic[i] = in.readLine();
 		}
 		
+		System.out.println(solve(mosaic));
+	}
+	
+	private static int solve(String[] mosaic) {
+		int ways = 1, segmentStart;
+		
+		for (String row : mosaic) {
+			int j = 0;
+			
+			while (j < row.length()) {
+				char currChar = row.charAt(j);
+				
+				if (currChar == '.') {
+					j++;
+					continue;
+				}
+				
+				segmentStart = j;
+				
+				do {
+					j++;
+				} while (j < row.length() && row.charAt(j) == currChar);
+				
+				ways *= ways(j - segmentStart);
+			}
+		}
+		
+		return ways;
 	}
 	
 	private static int ways(int length) {
-		if (length == 0) {
-			return 0;
-		}
-		
-		int ways = 0;
-		
-		for (int i = 0; i < LEGO_LENGTHS.length; i++) {
-			if (LEGO_LENGTHS[i] > length) {
-				continue;
+		while (highestLengthCalculated < length) {
+			highestLengthCalculated++;
+			for (int legoLength : LEGO_LENGTHS) {
+				if (legoLength <= highestLengthCalculated) {
+					ways[highestLengthCalculated] += ways[highestLengthCalculated - legoLength];
+				}
 			}
-			ways += ;
 		}
-		
-		return ways(length - 1) + 
-		
-		return 0;
-	}
-	
-	private static int solveRecursive() {
-	
+		return ways[length];
 	}
 	
 }
