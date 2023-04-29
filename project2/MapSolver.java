65e5244d7299ad636ff86a89cea8401be63cb5f3
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-04-29 22:07:30 +0100

    project2 refactored

--- project2/MapSolver.java
+++ project2/MapSolver.java
@@ -4,6 +4,13 @@ import java.util.Queue;
 
 public class MapSolver {
 	
+	private static final int[][] directions = {
+			{-1, 0},
+			{1, 0},
+			{0, -1},
+			{0, 1},
+	};
+	
 	private final char[][] map;
 	
 	public MapSolver(char[][] map) {
@@ -16,29 +23,24 @@ public class MapSolver {
 		
 		Queue<int[]> queue = new LinkedList<>();
 		int[][] numMoves = new int[map.length][map[0].length];
-		queue.add(coords);
 		numMoves[coords[0]][coords[1]] = 1;
+		queue.add(coords);
 		
 		while (!queue.isEmpty()) {
 			int[] currCoords = queue.poll();
-			int[][] nextCoords = new int[4][2];
-			nextCoords[0] = exploreDirection(currCoords[0], currCoords[1], -1, 0);
-			nextCoords[1] = exploreDirection(currCoords[0], currCoords[1], 1, 0);
-			nextCoords[2] = exploreDirection(currCoords[0], currCoords[1], 0, -1);
-			nextCoords[3] = exploreDirection(currCoords[0], currCoords[1], 0, 1);
 			
 			for (int i = 0; i < 4; i++) {
-				int[] iCoords = nextCoords[i];
+				int[] nextCoords = exploreDirection(currCoords[0], currCoords[1], directions[i][0], directions[i][1]);
 				
-				if (iCoords[0] == -1) {
+				if (nextCoords[0] == -1) {
 					continue;
 				}
-				if (map[iCoords[0]][iCoords[1]] == 'H') {
+				if (map[nextCoords[0]][nextCoords[1]] == 'H') {
 					return numMoves[currCoords[0]][currCoords[1]];
 				}
-				if (numMoves[iCoords[0]][iCoords[1]] == 0) {
-					queue.add(iCoords);
-					numMoves[iCoords[0]][iCoords[1]] = numMoves[currCoords[0]][currCoords[1]] + 1;
+				if (numMoves[nextCoords[0]][nextCoords[1]] == 0) {
+					queue.add(nextCoords);
+					numMoves[nextCoords[0]][nextCoords[1]] = numMoves[currCoords[0]][currCoords[1]] + 1;
 				}
 			}
 		}
@@ -48,16 +50,15 @@ public class MapSolver {
 	
 	private int[] exploreDirection(int x, int y, int dirX, int dirY) {
 		while (x + dirX >= 0 && x + dirX < map.length && y + dirY >= 0 && y + dirY < map[0].length) {
+			if (map[x][y] == 'H') {
+				return new int[]{x, y};
+			}
 			if (map[x + dirX][y + dirY] == 'O') {
 				return new int[]{x, y};
 			}
 			
 			x += dirX;
 			y += dirY;
-			
-			if (map[x][y] == 'H') {
-				return new int[]{x, y};
-			}
 		}
 		
 		return new int[]{-1, -1};
