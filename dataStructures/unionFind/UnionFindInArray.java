313111904dd0ee20b12ef47b313124eb28c39a78
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-05-15 18:01:57 +0100

    project3 progress, UnionFindInArray pull changes undone

--- dataStructures/unionFind/UnionFindInArray.java
+++ dataStructures/unionFind/UnionFindInArray.java
@@ -33,19 +33,6 @@ public class UnionFindInArray implements UnionFind {
         return partition[element] < 0;
     }
 
-
-    // Returns the representative of the set that contains 
-    // the specified element.
-    //
-    // Without side effects - Recursive.
-    public int find( int element ) throws InvalidElementException {
-        if ( !this.isInTheDomain(element) )
-            throw new InvalidElementException(validRangeMsg);
-
-        return this.findRec(element);
-    }
-
-
     // Pre-condition: 0 <= element < partition.length.
     protected int findRec( int element ) {
         if ( partition[element] < 0 )
@@ -53,22 +40,6 @@ public class UnionFindInArray implements UnionFind {
 
         return this.findRec( partition[element] );
     }
-
-
-    // Returns the representative of the set that contains 
-    // the specified element.
-    //
-    // Without side effects - Iterative.
-    public int find( int element ) throws InvalidElementException {
-        if ( !this.isInTheDomain(element) )
-            throw new InvalidElementException(validRangeMsg);
-
-        int node = element;
-        while ( partition[node] >= 0 )
-            node = partition[node];
-        return node;
-    }
-
 
     // Returns the representative of the set that contains 
     // the specified element.
@@ -120,35 +91,6 @@ public class UnionFindInArray implements UnionFind {
             partition[rep1] = rep2;
         }
     }
-
-
-    // Removes the two distinct sets S1 and S2 whose representatives are 
-    // the specified elements, and inserts the set S1 U S2.
-    // The representative of the new set S1 U S2 can be any of its members.
-    //
-    // Union by height or by rank.
-    public void union( int rep1, int rep2 ) throws InvalidElementException, 
-        NotRepresentativeException, EqualSetsException {  
-        if ( !this.isInTheDomain(rep1) || !this.isInTheDomain(rep2) )
-            throw new InvalidElementException(validRangeMsg);
-        if ( !this.isRepresentative(rep1) )
-            throw new NotRepresentativeException("First argument");
-        if ( !this.isRepresentative(rep2) )
-            throw new NotRepresentativeException("Second argument");
-        if ( rep1 == rep2 )
-            throw new EqualSetsException("The two arguments are equal");
-
-        if ( partition[rep1] <= partition[rep2] ) {
-            // Height(S1) >= Height(S2).
-            if ( partition[rep1] == partition[rep2] )
-                partition[rep1]--;
-            partition[rep2] = rep1;
-        }
-        else
-            // Height(S1) < Height(S2).
-            partition[rep1] = rep2;
-    }
-
 
 }
 
