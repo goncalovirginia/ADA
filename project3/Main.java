80beb4d9808e5f628d5414ad89cd618b7a18cfcd
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-05-16 02:24:17 +0100

    project3 simple Edmonds-Karp algorithm (with multiple sources and nodes with weight) solution implemented

--- project3/Main.java
+++ project3/Main.java
@@ -8,19 +8,19 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int[] numRegionsNumLinks = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int[][] regions = new int[numRegionsNumLinks[0] + 1][], links = new int[numRegionsNumLinks[1] + 1][];
+		int[][] regions = new int[numRegionsNumLinks[0]][], links = new int[numRegionsNumLinks[1]][];
 		
-		for (int i = 1; i <= numRegionsNumLinks[0]; i++) {
+		for (int i = 0; i < numRegionsNumLinks[0]; i++) {
 			regions[i] = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		}
 		
-		for (int i = 1; i <= numRegionsNumLinks[1]; i++) {
+		for (int i = 0; i < numRegionsNumLinks[1]; i++) {
 			links[i] = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		}
 		
 		int safeRegion = Integer.parseInt(in.readLine());
 		
-		System.out.println(new RescueByRail(regions, links, safeRegion).solve());
+		System.out.println(new RescueByRail(regions, links).solve(safeRegion));
 	}
 	
 }
--- project3/RescueByRail.java
+++ project3/RescueByRail.java
@@ -1,35 +1,86 @@
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class RescueByRail {
 	
-	LinkedList<int[]>[] outEdges;
+	LinkedList<int[]>[] nodeOutEdges;
 	
-	public RescueByRail(int[][] regions, int[][] links, int safeRegion) {
-		outEdges = new LinkedList[regions.length * 2];
-		outEdges[0] = new LinkedList<>();
-		
-		int i = 1;
+	public RescueByRail(int[][] regions, int[][] links) {
+		nodeOutEdges = new LinkedList[regions.length * 2 + 1];
+		nodeOutEdges[0] = new LinkedList<>();
+
+		int currNode = 1;
 		for (int[] region : regions) {
-			outEdges[0].add(new int[]{i, Integer.MAX_VALUE});
-			outEdges[i] = new LinkedList<>();
-			outEdges[i].add(new int[]{i + 1, Math.min(0, region[1] - region[0])});
-			outEdges[i+1] = new LinkedList<>();
-			
-			i += 2;
+			nodeOutEdges[currNode] = new LinkedList<>();
+			nodeOutEdges[currNode + 1] = new LinkedList<>();
+
+			nodeOutEdges[0].add(new int[]{currNode, region[0]});
+			nodeOutEdges[currNode].add(new int[]{0, 0});
+
+			nodeOutEdges[currNode].add(new int[]{currNode + 1, region[1]});
+			nodeOutEdges[currNode + 1].add(new int[]{currNode, 0});
+
+			currNode += 2;
 		}
 		
 		for (int[] link : links) {
-			outEdges[link[0] + 1].add(new int[]{link[1], Integer.MAX_VALUE});
+			nodeOutEdges[link[0] * 2].add(new int[]{link[1] * 2 - 1, Integer.MAX_VALUE});
+			nodeOutEdges[link[1] * 2].add(new int[]{link[0] * 2 - 1, Integer.MAX_VALUE});
+		}
+	}
+	
+	public int solve(int sink) {
+		sink = sink * 2 - 1;
+		int[][] flow = new int[nodeOutEdges.length][nodeOutEdges.length];
+		int[] parent = new int[nodeOutEdges.length];
+		int maxFlow = 0, increment;
+
+		while ((increment = findPath(flow, sink, parent)) != 0) {
+			maxFlow += increment;
+			int currNode = sink, currParent;
+
+			while (currNode != 0) {
+				currParent = parent[currNode];
+				flow[currParent][currNode] += increment;
+				flow[currNode][currParent] -= increment;
+				currNode = currParent;
+			}
 		}
 
-		outEdges[safeRegion].add(new int[]{i, Integer.MAX_VALUE});
-
-		System.out.println(Arrays.toString(outEdges));
+		return maxFlow;
 	}
-	
-	public int solve() {
+
+	private int findPath(int[][] flow, int sink, int[] parent) {
+		Queue<Integer> queue = new LinkedList<>();
+		boolean[] explored = new boolean[nodeOutEdges.length];
+		int[] pathIncrement = new int[nodeOutEdges.length];
+		queue.add(0);
+		explored[0] = true;
+		pathIncrement[0] = Integer.MAX_VALUE;
+
+		while (!queue.isEmpty()) {
+			int currNode = queue.poll();
+
+			for (int[] edge : nodeOutEdges[currNode]) {
+				int nextNode = edge[0];
+				int residue = edge[1] - flow[currNode][nextNode];
+
+				if (!explored[nextNode] && residue > 0) {
+					parent[nextNode] = currNode;
+					pathIncrement[nextNode] = Math.min(pathIncrement[currNode], residue);
+
+					if (nextNode == sink) {
+						return pathIncrement[nextNode];
+					}
+
+					explored[nextNode] = true;
+					queue.add(nextNode);
+				}
+			}
+		}
+
 		return 0;
 	}
-	
+
 }
